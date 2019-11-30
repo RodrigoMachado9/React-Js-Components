a679fb1e028ccdfdd150f2cab8d538e530786923
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-11-30 14:34:26 -0300

    criando componentes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import MeuComponente from "./components/MeuComponente";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <MeuComponente/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MeuComponente.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Function components ,
+// Sempre deverá retornar algum elemento. como no exemplo abaixo => <h1></h1>
+
+// function MeuComponente(){
+//     return <h1>HelloWorl</h1>;
+// }
+
+// Outra forma de construir determinado componente
+const MeuComponente = () => {
+    return <h1>Hello_World</h1>;
+
+};
+
+export default  MeuComponente;
