ac69d366b84471dc89231bdea61d556fd380f7d0
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-07 20:44:16 -0300

    working with setInterval and setState

--- src/components/Time.js
+++ src/components/Time.js
@@ -10,12 +10,24 @@ class Time extends Component{
         super(props);
         this.state = INITIAL_STATE;
 
+        // utilizando o setState a variavel time será atualizada com um novo valor, alterando o seu stado inicial = 7
         // setTimeout(()=>{
         //     this.setState({
         //         time:5
         //     })
         // }, 1000);
 
+        // setInterval > sera iniciado a cada 1 segundo;
+        // para o setState atualizar o stado atual da variavel conforme intervalo, é necessario declarar uma função pra o setState.
+        setInterval(()=>{
+            this.setState(( state , props)=>{
+                return {
+                    time: state.time + 1
+                }
+            })
+
+
+        }, 1000)
 
     }
     render() {
