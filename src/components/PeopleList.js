08a7bed875d3524f138d7d82ac2b5565e09f7c4a
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-12 22:30:42 -0300

    include method onClick and function sayMyName

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 
 
 const myList = [
-    {'id':'12345', 'nome':'rodrigo', 'idade':'24'},
-    {'id':'12346', 'nome':'aioria', 'idade':'25'},
-    {'id':'12347', 'nome':'aioros', 'idade':'26'},
-    {'id':'12348', 'nome':'shaka', 'idade':'32'},
-    {'id':'12349', 'nome':'kamus', 'idade':'40'},
-    {'id':'12344', 'nome':'doku', 'idade':'80'},
+    {'id':'12345', 'name':'rodrigo', 'age':'24'},
+    {'id':'12346', 'name':'aioria', 'age':'25'},
+    {'id':'12347', 'name':'aioros', 'age':'26'},
+    {'id':'12348', 'name':'shaka', 'age':'32'},
+    {'id':'12349', 'name':'kamus', 'age':'40'},
+    {'id':'12344', 'name':'doku', 'age':'80'},
 
 
 ];
@@ -20,20 +20,33 @@ class PeopleList extends Component{
     //
     // }
 
+    // sayMyName(person)
+
+    sayMyName(person){
+        // console.log(person)
+        alert(person.name)
+
+    }
     render() {
 
         // const elementsList = [];
         // for (let i = 0 ; i < myList.length;  i ++ ){
         //     elementsList.push(
-        //         <li>{myList[i].nome} - {myList[i].idade} anos</li>
+        //         <li>{myList[i].nome} - {myList[i].age} anos</li>
         //     )
         //
         // }
-        const list = myList.map(person => <li key={person.id}>{person.nome} - {person.idade} anos</li>);
+
+        // const list = myList.map(person => <li key={person.id}>{person.name} - {person.age} anos</li>);
+
         return(
             // utilizando a função => map / array function para 'substituir' o comando for;
             <ul>
-                {list}
+                {/*{list}*/}
+                {
+                    myList.map(person => <li onClick={this.sayMyName.bind(this, person)} key={person.id}> {person.name} - {person.age} </li>)
+
+                }
             </ul>
         );
     }
