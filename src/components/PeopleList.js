e322d09cd576888a509e106e8e6ae9df6ecd01a2
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-12 22:13:35 -0300

    add fild (id) for using key attribuite

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 
 
 const myList = [
-    {'nome':'rodrigo', 'idade':'24'},
-    {'nome':'aioria', 'idade':'25'},
-    {'nome':'aioros', 'idade':'26'},
-    {'nome':'shaka', 'idade':'32'},
-    {'nome':'kamus', 'idade':'40'},
-    {'nome':'doku', 'idade':'80'},
+    {'id':'12345', 'nome':'rodrigo', 'idade':'24'},
+    {'id':'12346', 'nome':'aioria', 'idade':'25'},
+    {'id':'12347', 'nome':'aioros', 'idade':'26'},
+    {'id':'12348', 'nome':'shaka', 'idade':'32'},
+    {'id':'12349', 'nome':'kamus', 'idade':'40'},
+    {'id':'12344', 'nome':'doku', 'idade':'80'},
 
 
 ];
@@ -29,7 +29,7 @@ class PeopleList extends Component{
         //     )
         //
         // }
-        const list = myList.map(person => <li>{person.nome} - {person.idade} anos</li>);
+        const list = myList.map(person => <li key={person.id}>{person.nome} - {person.idade} anos</li>);
         return(
             // utilizando a função => map / array function para 'substituir' o comando for;
             <ul>
